792848375032215eb6ec094fa35cbb4c53ee07cb
dreamtofparrish <168297527+dreamtofparrish@users.noreply.github.com>
2024-04-28 00:38:16 +0300

    Add files via upload

--- func.py
+++ func.py
@@ -16,8 +16,14 @@ bot = tweepy.Client(
     access_token_secret= os.environ['ACCESS_TOKEN_SECRET'])
 
 def post_quote():
-	quotes = data["quotes"]
+	key = random.choice(list(data.keys()))
+	quotes = data[key]
 	random_index = random.randint(0, len(quotes)-1)
-	quote = post_quote[random_index]
-	r = bot.create_tweet(text=quote)
+	quote = quotes[random_index]
+	try:
+		r = bot.create_tweet(text=quote)
+	except:
+		random_index = random.randint(0, len(quotes)-1)
+		quote = quotes[random_index]
+		r = bot.create_tweet(text=quote)
 	return None
